3b86f7cd200d79597f6b2759de85f85ed53f9467
joaovitor <joaovitor@klever.io>
2021-03-23 17:12:02 -0300

    23-03

--- javascript-logica/aulaJS029-ternario/index.js
+++
@@ -1,6 +0,0 @@
-// ternario o ternario em js é o seguinte 
-// são dois pontos e uma interrogação
-// pra que é usado é para fazer um if else em uma unica linha 
-
-// exemplo
-//  ? 'Acontece uma coisa' : 'Acontece outra coisa'
