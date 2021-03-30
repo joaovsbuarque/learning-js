54e58255facee035c51d74fc7590e3437ee6cb02
joaovitor <joaobuarqueestudos@gmail.com>
2021-03-30 10:39:57 -0300

    for in

---
+++ javascript-logica/aulajs036-for-in/index.js
@@ -0,0 +1,18 @@
+const frutas = ['Pera', 'Maça', 'Uva'];
+
+for ( let i in frutas) {
+    console.log(frutas[i])
+}
+
+// for in -> Esse for lê o indice ou chaves dos objetos
+// outro exemplo agora com objetos 
+
+const pessoa = {
+    nome: 'João',
+    sobrenome: 'Buarque',
+    idade:24,
+};
+
+for (let i in pessoa) {
+    console.log(i)
+}
