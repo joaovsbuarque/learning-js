dd3c2e983b16135731be008e114b1e98b14459b9
joaovitor <joaobuarqueestudos@gmail.com>
2021-04-05 22:32:21 -0300

    exercicios para treinar ternarios e arrow function

---
+++ javascript-logica/aulajs042-ex2/index.js
@@ -0,0 +1,7 @@
+// Escreva a função ePaisagem que recebe dois argumentos
+// largura e altura de uma imagem (number).
+// Retorna true se a imagem estiver no modo paisagem
+
+const ePaisagem = (largura, altura) => largura > altura;
+
+console.log(ePaisagem(1080, 1920))
