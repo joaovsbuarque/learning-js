354ef8f5d002d65907659573d3942fac6ecaee3e
joaovitor <joaobuarqueestudos@gmail.com>
2021-04-15 10:58:08 -0300

    concatenacao e spread

---
+++ javascript-Arrays/aulajs063-concatenando-arrays/index.js
@@ -0,0 +1,10 @@
+// concatenando arrays é feito atraves da função .concat() ou atraves ...
+
+const arr1 = [1,2,3];
+const arr2 = [4,5,6];
+// const arr3 = arr1.concat(arr2) // -> com a função concat
+// const arr3 = [...arr1,...arr2] // -> com o spread
+
+// const arr3 = arr1.concat(arr2,'Luiz', [7, 8, 9]) // -> adicionando
+// const arr3 = [...arr1,'Luiz' ,...arr2, ...[7, 8, 9]] // -> adicionando
+console.log(arr3)
